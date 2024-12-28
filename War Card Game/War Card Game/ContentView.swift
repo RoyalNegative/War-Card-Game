4240d8155298230b7582e3c1feaf8823551c3362
Kaan <103496597+RoyalNegative@users.noreply.github.com>
2024-12-28 21:12:13 +0300

    added the new file

--- War Card Game/War Card Game/ContentView.swift
+++ War Card Game/War Card Game/ContentView.swift
@@ -6,32 +6,43 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    @State var playerCard = "card2"
+    @State var cpuCard = "card3"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
        
         ZStack{
             
-            Image("background-cloth")
+            Image("background-wood")
                 .resizable()
                 .ignoresSafeArea()
+                .overlay(
+                    Color.black.opacity(0.1)
+                )
+                
             VStack{
                 Spacer()
                 Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
                 Spacer()
                 HStack{
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                     
                 }
                 Spacer()
                 Button{
-                    for i in 0..<5{
-                        deal()
-                    }
+                   deal()
                     
                 } label: {
                     Image("button")
@@ -48,20 +59,54 @@ struct ContentView: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Text("0")
+                    Text(String(playerScore))
                     Spacer()
-                    Text("0")
+                    Text(String(cpuScore))
                     Spacer()
                 }.foregroundColor(.white).font(.largeTitle)
                 Spacer()
                 Spacer()
-                
+                Button("Reset Game") {
+                    resetGame()
+                }.foregroundColor(.white).font(.title)
+                Spacer()
             }
         }
     }
     func deal(){
-        print("Deal")
+        // Randomize the player cards
+        var playerCardNumber = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardNumber)
+
+        
+        // Randomize the cpu card
+        var cpuCardNumber = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardNumber)
+        
+        //Calculate the score
+        if playerCardNumber > cpuCardNumber{
+            playerScore += 1
+        }
+        else if playerCardNumber < cpuCardNumber{
+            cpuScore += 1
+        }
+        else {
+            let alert = UIAlertController(title: "It's a Draw!",
+                                              message: "No Score Added.",
+                                          preferredStyle: .alert)
+        }
+        
     }
+    
+    
+    func resetGame(){
+        playerCard = ""
+        cpuCard = ""
+        playerScore = 0
+        cpuScore = 0
+    }
+    
+    
     
 }
 
